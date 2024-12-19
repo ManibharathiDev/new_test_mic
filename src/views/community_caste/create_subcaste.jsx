6690df4da5603d5c00153bb96a14fcfb082c0966
Manibharathi <manibharath159@gmail.com>
2024-12-19 20:19:14 +0530

    Caste,Community and Subcaste UI

--- src/views/community_caste/create_subcaste.jsx
+++ src/views/community_caste/create_subcaste.jsx
@@ -1,7 +1,129 @@
+import React from 'react';
+import { useState,useEffect } from 'react';
+import axios from "axios";
+import { Row, Col, Card, Table, Tabs, Tab,Button, OverlayTrigger, Tooltip, ButtonToolbar, Dropdown, DropdownButton, SplitButton, CardBody, Form } from 'react-bootstrap';
+import secureLocalStorage from 'react-secure-storage';
 const CreateSubCaste = () =>{
-    return(
-        <>
-        </>
+    let token = "";
+  let bearer = ""
+  if(secureLocalStorage.getItem("STATUS") != null)
+    {
+        const data = JSON.parse(secureLocalStorage.getItem("STATUS"));
+        if(!data.status)
+        {
+          window.location.replace("/admin/login");
+        }
+        token = data.token;
+        bearer = 'Bearer '+token;
+    }
+    else{
+      window.location.replace("/admin/login");
+    }
+    const [caste,setCaste] = useState([]);
+    const [data, setData] = useState({
+        caste_id:"",
+        name:"",
+      });
+      const handleChange = (e) =>{
+        const value = e.target.value
+        setData({
+          ...data,[e.target.name]:e.target.value
+        })
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const userData = {
+            country_code: data.country_code,
+          name: data.name,
+        };
+        const headers = { 'Authorization': bearer };
+        let URL = window.API_URL+"caste";
+        axios.post(URL,userData,{headers})
+        .then((response)=>{
+          console.log(response);
+          console.log(response.data.status, response.data.message);
+            if(response.data.status == true)
+            {
+              setData({
+                country_code: "",
+                name:""
+              });
+            }
+        });
+    }
+
+    const fetchCaste = async () =>{
+        try {
+            const headers = { 'Authorization': bearer };
+            let URL = window.API_URL+"caste/fetch_all";
+            const response = await axios.get(URL,{ headers });
+            setCaste(response.data.result);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const renderCaste = () =>{
+        return caste?.map((cast,index) => (
+          <option value={cast.id} key={cast.id}>
+              {cast.name}
+          </option>
+      ))
+      }
+
+    useEffect(()=> {
+        fetchCaste();
+    }, []);
+
+    return (
+      <React.Fragment>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Header>
+                <Card.Title as="h5">Create New Subcaste</Card.Title>
+               
+              </Card.Header>
+                 <CardBody>
+                 <Form onSubmit={handleSubmit}>  
+                        <Row>
+                        <Col md={6}>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlSelect1">
+                                                        <Form.Label>Caste</Form.Label>
+                                                        <Form.Control name="caste_id" as="select" value={data.caste_id} onChange={handleChange}>
+                                                         <option value="">Select Caste</option>
+                                                          {
+                                                            renderCaste()
+                                                          }
+                                                        </Form.Control>
+                                                      </Form.Group>
+                            </Col>
+                            <Col md={6}>
+                            <Form.Group className="mb-3" controlId="formNameParty">
+                                <Form.Label>Name of the Subcaste</Form.Label>
+                                <Form.Control type="text" value={data.name} name='name' onChange={handleChange} placeholder="Enter the name of the Party" />
+                                
+                            </Form.Group>
+                            </Col>
+                            
+                        </Row>
+
+                        <Row>
+                            <Col md={12}>
+                                <Button type="submit" className="text-capitalize btn btn-primary">Save</Button>
+                                <Button type="reset" className="text-capitalize btn btn-secondary">Clear</Button>
+                            </Col>
+                        </Row>
+
+
+                    </Form>
+                 </CardBody>   
+            </Card>
+            
+            
+          </Col>
+        </Row>
+      </React.Fragment>
     );
 }
 export default CreateSubCaste;
--- src/views/community_caste/view_caste.jsx
+++ src/views/community_caste/view_caste.jsx
@@ -1,7 +1,110 @@
+import React from 'react';
+import { useState,useEffect } from 'react';
+import axios from "axios";
+import { Row, Col, Card, Table, Tabs, Tab,Button, OverlayTrigger, Tooltip, ButtonToolbar, Dropdown, DropdownButton, SplitButton, CardBody, Form } from 'react-bootstrap';
+import secureLocalStorage from 'react-secure-storage';
+import { Link } from 'react-router-dom';
 const ViewCaste = () =>{
-    return(
-        <>
-        </>
+  let token = "";
+  let bearer = ""
+  if(secureLocalStorage.getItem("STATUS") != null)
+    {
+        const data = JSON.parse(secureLocalStorage.getItem("STATUS"));
+        if(!data.status)
+        {
+          window.location.replace("/admin/login");
+        }
+        token = data.token;
+        bearer = 'Bearer '+token;
+    }
+    else{
+      window.location.replace("/admin/login");
+    }
+    
+    const [caste,setCaste] = useState([]);
+    const [page,setPage] = useState("1");
+    
+
+      const fetchCaste = async(page) =>{
+       
+        try {
+            const headers = { 'Authorization': bearer };
+            let URL = window.API_URL+"caste?page="+page;
+            const response = await axios.get(URL,{ headers });
+            setCaste(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+      }
+    const renderHeader = () => {
+      let headerElement = ['#', 'Caste','action']
+    
+      return headerElement.map((key, index) => {
+          return <th key={index}>{key.toUpperCase()}</th>
+      })
+    }
+
+    const deleteLoksaba = (id,idx)=>{
+      const headers = { 'Authorization': bearer };
+      let URL = window.API_URL+"constituency/delete/"+id;
+      axios.delete(URL,{ headers })  
+    .then(res => {  
+      setConstituency((data) =>
+        data.filter((item) => item.id !== id));
+    })  
+    }
+
+    const renderBody = () => {
+      return caste.data?.map((data,index) => (
+        <tr key={data.id}>
+             <td>{index+1}</td>
+            <td>{data.name}</td>
+                    <td>
+            <Link to={`../app/assembly/edit/${data.id}`} className="label theme-bg2 text-white f-12">
+            <i className='feather icon-edit'></i> Edit
+            </Link>
+              <Link to="#" onClick={()=>deleteLoksaba(data.id,index)} className="label theme-bg text-c-red  f-12">
+            <i className='feather icon-delete'></i> Delete
+        </Link>
+            </td>
+        </tr>
+    ))
+    }
+
+  useEffect(()=> {
+    if(page)
+        fetchCaste();
+    }, [page]);
+    return (
+      <React.Fragment>
+
+
+        <Row>
+          <Col>
+            <Card>
+              <Card.Header>
+                <Card.Title as="h5">Caste Details</Card.Title>
+              </Card.Header>
+              <Card.Body>
+              <Table responsive>
+                  <thead>
+                    <tr>
+                      {renderHeader()}
+                    </tr>
+                  </thead>
+                  <tbody>
+                        {
+                              renderBody()
+                        }
+                </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+            
+            
+          </Col>
+        </Row>
+      </React.Fragment>
     );
-}
+  };
 export default ViewCaste;
